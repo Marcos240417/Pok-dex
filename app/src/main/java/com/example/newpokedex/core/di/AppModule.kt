d5b2a846175809c6fbf035621dee58810c9db5e0
Estudo Kotlin Android <mr6227017@gmail.com>
2026-02-16 12:53:42 -0300

    feat(core): configurar Module e Application para injeção de dependências
    - Criado módulo de dependências (Module) para fornecer instâncias de Repository, Service e Database
    - Configurado Application para inicializar o Hilt/Dagger e garantir ciclo de vida adequado das dependências
    - Essa configuração centraliza a criação de objetos e facilita o uso de injeção de dependência em toda a aplicação
    - Estrutura preparada para escalabilidade, manutenção e testes unitários mais simples

---
+++ app/src/main/java/com/example/newpokedex/core/di/AppModule.kt
@@ -0,0 +1,48 @@
+package com.example.newpokedex.core.di
+
+import com.example.newpokedex.core.data.local.PokemonDatabase
+import com.example.newpokedex.core.domain.repository.PokemonRepository
+import com.example.newpokedex.core.domain.repository.PokemonRepositoryImpl
+import com.example.newpokedex.core.remote.PokeApiService
+import com.example.newpokedex.feature.PokemonViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+val databaseModule = module {
+    // Cria a instância do Banco de Dados
+    single {
+        PokemonDatabase.getDatabase(androidContext())
+    }
+
+
+    // Cria o DAO a partir da instância do banco acima
+    single { get<PokemonDatabase>().pokemonDao() }
+}
+
+val repositoryModule = module {
+    // Injeta automaticamente a API (do networkModule) e o DAO (do databaseModule)
+    single<PokemonRepository> {
+        PokemonRepositoryImpl(api = get(), dao = get())
+    }
+}
+
+val viewModelModule = module {
+    // O Koin injeta o 'repository' que foi definido no 'repositoryModule'
+    viewModel { PokemonViewModel(repository = get()) }
+}
+
+
+    // 3. Provedor do Retrofit e da PokeApiService
+val networkModule = module {
+    single {
+        Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PokeApiService::class.java)
+        }
+    }
+
---
+++ app/src/main/java/com/example/newpokedex/core/di/PokedexApplication.kt
@@ -0,0 +1,25 @@
+package com.example.newpokedex.core.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.GlobalContext.startKoin
+
+class PokedexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@PokedexApplication)
+
+            // Aqui você carrega todas as suas variáveis de módulo separadamente
+            modules(
+                networkModule,
+                databaseModule,
+                repositoryModule,
+                viewModelModule
+            )
+        }
+    }
+}
